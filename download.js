c30b59d4d7958d977f1b9c6444e3a113a605b617
koru1130 <me@koru.me>
2022-08-18 05:32:31 +0800

    auto create raw folder if doesn't exist

--- download.js
+++ download.js
@@ -3,14 +3,14 @@ import fs from "fs";
 import { readdir } from 'node:fs/promises';
 import pMap from 'p-map'
 
-const url = 
-    pageNumber => `https://nol.ntu.edu.tw/nol/coursesearch/search_for_02_dpt.php?alltime=yes&allproced=yes&selcode=-1&dptname=0&coursename=&teachername=&current_sem=111-1&yearcode=0&op=&startrec=${pageNumber*150}&week1=&week2=&week3=&week4=&week5=&week6=&proced0=&proced1=&proced2=&proced3=&proced4=&procedE=&proced5=&proced6=&proced7=&proced8=&proced9=&procedA=&procedB=&procedC=&procedD=&allsel=yes&selCode1=&selCode2=&selCode3=&page_cnt=150`
+const url = sem =>
+    pageNumber => `https://nol.ntu.edu.tw/nol/coursesearch/search_for_02_dpt.php?alltime=yes&allproced=yes&selcode=-1&dptname=0&coursename=&teachername=&current_sem=${sem}&yearcode=0&op=&startrec=${pageNumber*150}&week1=&week2=&week3=&week4=&week5=&week6=&proced0=&proced1=&proced2=&proced3=&proced4=&procedE=&proced5=&proced6=&proced7=&proced8=&proced9=&procedA=&procedB=&procedC=&procedD=&allsel=yes&selCode1=&selCode2=&selCode3=&page_cnt=150`
 
 const getPage = 
     n => new Promise((resolve, reject) => {            
             console.log(`Page ${n} started...`);
             https.get(
-                url(n),
+                url('111-1')(n),
                 {rejectUnauthorized: false},
                 res => {
                     const path = `raw/${n}.html`;
@@ -30,6 +30,11 @@ const toN = n => [...Array(n).keys()];
 
 (async () => { 
     try {
+
+        if(!fs.existsSync("raw")) {
+            fs.mkdirSync("raw");
+        }
+
         const downloaded = await readdir('raw');
         
         const toDownload = toN(100).filter(n => !downloaded.includes(`${n}.html`));        
